3b1979f09853e22f66e07c9464ca7a84493170d9
ken1286 <kindle1286@gmail.com>
2019-12-29 09:06:12 -0800

    changed seeds

--- data/seeds/002_trucks.js
+++ data/seeds/002_trucks.js
@@ -4,25 +4,19 @@ exports.seed = function(knex) {
       operator_id: 1,
       cuisine: 'mexican',
       current_latitude: 35,
-      current_longitude: 40,
-      arrival_time: Date.now(),
-      departure_time: Date.now()
+      current_longitude: 40
     },
     {
       operator_id: 1,
       cuisine: 'chinese',
       current_latitude: 20,
-      current_longitude: 45,
-      arrival_time: Date.now(),
-      departure_time: Date.now()
+      current_longitude: 45
     },
     {
       operator_id: 2,
       cuisine: 'italian',
       current_latitude: 25,
-      current_longitude: 50,
-      arrival_time: Date.now(),
-      departure_time: Date.now()
+      current_longitude: 50
     }
   ]);
 };
